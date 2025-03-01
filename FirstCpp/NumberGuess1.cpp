e2eb9b5396693ca99b45f49347082dc2e8b4bcab
한강 장 <skijh5@naver.com>
2025-03-01 16:48:01 +0900

    숫자 추론 코드 수정

--- FirstCpp/NumberGuess1.cpp
+++ FirstCpp/NumberGuess1.cpp
@@ -12,21 +12,37 @@ int main() {
 
 	int number = distrib(gen);
 
-	int input_num;
-	bool isOk = false;
+	//int input_num;
+	//bool isOk = false;
 
-	while (!isOk) {
+	//while (!isOk) {
+	//	cout << "입력: ";
+	//	cin >> input_num;
+
+	//	if(input_num > number)
+	//		cout << "너무 커요!" << endl;
+	//	else if (input_num < number)
+	//		cout << "너무 작아요!" << endl;
+	//	else {
+	//		cout << "정답입니다!" << endl;
+	//		cout << "number: " << number << ", input_num: " << input_num << endl;
+	//		isOk = true;
+	//	}
+	//}
+
+	while (1) {
+		int input_num;
 		cout << "입력: ";
 		cin >> input_num;
 
 		if(input_num > number)
-			cout << "너무 커요!" << endl;
+			cout << "너무 커요! 차이나는 크기: " << abs(number - input_num) << endl;
 		else if (input_num < number)
-			cout << "너무 작아요!" << endl;
+			cout << "너무 작아요! 차이나는 크기: " << abs(number - input_num) << endl;
 		else {
 			cout << "정답입니다!" << endl;
 			cout << "number: " << number << ", input_num: " << input_num << endl;
-			isOk = true;
+			break;
 		}
 	}
 
